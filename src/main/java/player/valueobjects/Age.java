9575189da7d8fc9083429afbdd79bd1f53bc8c39
leilaweicman <leilaweic@hotmail.com>
2025-11-19 11:52:52 +0100

    feat: add Email, NickName and Age as value objects with internal validation

---
+++ src/main/java/player/valueobjects/Age.java
@@ -0,0 +1,41 @@
+package player.valueobjects;
+
+import exceptions.ValidationException;
+
+public class Age {
+    private final int value;
+
+    public Age(int value) {
+        if (value <= 0) {
+            throw new ValidationException("Age must be greater than zero");
+        }
+        if (value < 12) {
+            throw new ValidationException("Player must be at least 12 years old to register");
+        }
+        if (value > 120) {
+            throw new ValidationException("Invalid age: must be less than 120");
+        }
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Age age)) return false;
+        return value == age.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(value);
+    }
+}
