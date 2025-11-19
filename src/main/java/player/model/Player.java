ca73e498f6adcdbc079629455aa4f52d5c7554d8
leilaweicman <leilaweic@hotmail.com>
2025-11-19 11:16:10 +0100

    feat: add Player model

--- src/main/java/player/model/Player.java
+++ src/main/java/player/model/Player.java
@@ -1,4 +1,47 @@
 package player.model;
 
 public class Player {
+    private int id;
+    private String nickName;
+    private String email;
+    private int age;
+
+    public Player() {}
+
+    public Player(int id, String nickName, String email, int age) {
+        this.id = id;
+        this.nickName = nickName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public int getId() { return id; }
+
+    public String getNickName() { return nickName; }
+
+    public String getEmail() { return email; }
+
+    public int getAge() { return age; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Player player)) return false;
+        return id == player.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", name='" + nickName + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
