0fb7b458e0cfde96467501d91e6fdeb2e87c2d11
Esther <githubesther@gmail.com>
2025-11-20 13:58:50 +0100

    FEAT: changes for the connection database and the init compose

--- src/main/java/databaseconnection/DatabaseConnection.java
+++ src/main/java/databaseconnection/DatabaseConnection.java
@@ -1,39 +1,11 @@
 package databaseconnection;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
-public class DatabaseConnection {
+public interface DatabaseConnection {
+    void openConnection();
 
-    private static volatile DatabaseConnection instance;
-    private Connection connection;
+    Connection getConnection();
 
-    private static final String URL = "";
-    private static final String USER = "";
-    private static final String PASSWORD = "";
-
-    private DatabaseConnection() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException("Error connecting to the database", e);
-        }
-    }
-
-    public static DatabaseConnection getInstance() {
-        if (instance == null) {
-            synchronized (DatabaseConnection.class) {
-                if (instance == null) {
-                    instance = new DatabaseConnection();
-                }
-            }
-        }
-        return instance;
-    }
-
-    public Connection getConnection() {
-        return connection;
-    }
+    void closeConnection();
 }
