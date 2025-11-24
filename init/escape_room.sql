8bb731b9e0b05f5db143a1949f692edfa6999ccc
Esther <githubesther@gmail.com>
2025-11-24 13:47:30 +0100

    FEAT: adding the user-in-sql-file

--- init/escape_room.sql
+++ init/escape_room.sql
@@ -5,16 +5,18 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 
-CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
-
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
-
-FLUSH PRIVILEGES;
 -- -----------------------------------------------------
 -- Schema escape_room
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `escape_room` DEFAULT CHARACTER SET utf8 ;
 USE `escape_room` ;
+
+
+CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+
+GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%';
+
+FLUSH PRIVILEGES;
 
 -- -----------------------------------------------------
 -- Table `escape_room`.`room`
