75fe6d5d67a49e6d39a72cb61b1092f436a16499
leilaweicman <leilaweic@hotmail.com>
2025-11-27 11:13:10 -0300

    feat: implement registerPlayer() with uniqueness validation

--- src/main/java/player/service/PlayerService.java
+++ src/main/java/player/service/PlayerService.java
@@ -1,11 +1,27 @@
 package player.service;
 
+import exceptions.BusinessException;
 import player.dao.PlayerDao;
+import player.model.Player;
 
 public class PlayerService {
     private final PlayerDao playerDao;
 
     public PlayerService(PlayerDao playerDao) {
         this.playerDao = playerDao;
+    }
+
+    public void createPlayer(String nickName, String email) {
+        Player player = Player.create(nickName, email);
+
+        if (playerDao.findByNickName(nickName).isPresent()) {
+            throw new BusinessException("NickName already taken: " + nickName);
+        }
+
+        if (playerDao.findByEmail(email).isPresent()) {
+            throw new BusinessException("Email already registered: " + email);
+        }
+
+        playerDao.save(player);
     }
 }
