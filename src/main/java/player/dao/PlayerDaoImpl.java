fac7fd3aa4e94978b9ffc72a5ed766d6ac6a1f93
leilaweicman <leilaweic@hotmail.com>
2025-11-27 10:27:08 -0300

    feat: add DatabaseConnection

--- src/main/java/player/dao/PlayerDaoImpl.java
+++ src/main/java/player/dao/PlayerDaoImpl.java
@@ -1,11 +1,27 @@
 package player.dao;
 
+import databaseconnection.DatabaseConnection;
+import databaseconnection.MYSQLDatabaseConnection;
+import exceptions.DataAccessException;
 import player.model.Player;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public class PlayerDaoImpl implements PlayerDao {
+
+    private final DatabaseConnection dbConnection;
+
+    public PlayerDaoImpl() {
+        try {
+            this.dbConnection = MYSQLDatabaseConnection.getInstance();
+        } catch (SQLException | ClassNotFoundException | IOException e) {
+            throw new DataAccessException("Failed to initialize database connection", e);
+        }
+    }
+
     @Override
     public void save(Player entity) {
 
