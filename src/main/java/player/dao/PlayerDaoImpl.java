7351912fbe6b261f0ca3377971864c69af7b0587
leilaweicman <leilaweic@hotmail.com>
2025-11-27 10:34:39 -0300

    feat: implement save() and mapRow() methods for PlayerDaoImpl using Value Objects

--- src/main/java/player/dao/PlayerDaoImpl.java
+++ src/main/java/player/dao/PlayerDaoImpl.java
@@ -6,11 +6,22 @@ import exceptions.DataAccessException;
 import player.model.Player;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
+import java.sql.*;
 
 public class PlayerDaoImpl implements PlayerDao {
+
+    private static final String INSERT_SQL = """
+        INSERT INTO player (nick_name, email) VALUES (?, ?)
+        """;
+
+    private static final String SELECT_BY_ID = """
+        SELECT id_player, nick_name, email FROM player WHERE id_player = ?
+        """;
 
     private final DatabaseConnection dbConnection;
 
@@ -24,7 +35,19 @@ public class PlayerDaoImpl implements PlayerDao {
 
     @Override
     public void save(Player entity) {
+        dbConnection.openConnection();
+        try (Connection conn = dbConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(INSERT_SQL)) {
 
+            ps.setString(1, entity.getNickName().getValue());
+            ps.setString(2, entity.getEmail().getValue());
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Error saving player", e);
+        } finally {
+            dbConnection.closeConnection();
+        }
     }
 
     @Override
@@ -50,5 +73,13 @@ public class PlayerDaoImpl implements PlayerDao {
     @Override
     public Optional<Player> findByEmail(String email) {
         return Optional.empty();
+    }
+
+    private Player mapRow(ResultSet rs) throws SQLException {
+        return Player.rehydrate(
+                rs.getInt("id_player"),
+                rs.getString("nick_name"),
+                rs.getString("email")
+        );
     }
 }
