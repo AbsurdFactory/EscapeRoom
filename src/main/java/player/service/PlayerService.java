6e8ecea4834bb39a81b9b2e026999b805c2db4b1
leilaweicman <leilaweic@hotmail.com>
2025-11-27 11:05:43 -0300

    feat: PlayerService with dependency on PlayerDao

--- src/main/java/player/service/PlayerService.java
+++ src/main/java/player/service/PlayerService.java
@@ -1,4 +1,11 @@
 package player.service;
 
+import player.dao.PlayerDao;
+
 public class PlayerService {
+    private final PlayerDao playerDao;
+
+    public PlayerService(PlayerDao playerDao) {
+        this.playerDao = playerDao;
+    }
 }
