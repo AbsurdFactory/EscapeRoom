5f28a5e2dafd009478aebff92f48147503b40ddd
Esther <githubesther@gmail.com>
2025-11-28 16:11:17 +0100

    Feat: refactor value objects in ObjectDecorationDaoImpl.java and ObjectDecorationServiceTest.java and creating the tests for the methods in the InventoryServiceService

--- src/test/java/inventory/InventoryServiceTest.java
+++ src/test/java/inventory/InventoryServiceTest.java
@@ -13,47 +13,47 @@ class InventoryServiceTest {
     @Test
     @DisplayName("Return the price for all the clues in the database")
     void getCluesTotalPrice() {
-        assertTrue(inventoryService.getCluesTotalPrice().compareTo(BigDecimal.ZERO)> 0);
+        assertTrue(inventoryService.getCluesTotalPrice().compareTo(BigDecimal.ZERO)>= 0);
     }
 
     @Test
     @DisplayName("Return the price for all the object decorations in the database")
     void testGetObjectDecorationsTotalPrice() {
-        assertTrue(inventoryService.getObjectDecorationsTotalPrice().compareTo(BigDecimal.ZERO)> 0);
+        assertTrue(inventoryService.getObjectDecorationsTotalPrice().compareTo(BigDecimal.ZERO)>= 0);
 
     }
 
     @Test
     @DisplayName("Return the price for all the clues and object decorations in the database")
     void testGetTotalValueOfInventory() {
-        assertTrue(inventoryService.getTotalValueOfInventory().compareTo(BigDecimal.ZERO)> 0);
+        assertTrue(inventoryService.getTotalValueOfInventory().compareTo(BigDecimal.ZERO)>= 0 );
 
     }
 
     @Test
     @DisplayName("Return the total quantity of items in the database")
     void testGetAllItemsQuantitiesFromInventory() {
-        assertTrue(inventoryService.getAllItemsQuantitiesFromInventory()>0);
+        assertTrue(inventoryService.getAllItemsQuantitiesFromInventory()>=0);
     }
 
     @Test
     @DisplayName("Return the total quantity of rooms in the database")
     void testGetAllRoomsFromInventory() {
-        assertTrue(inventoryService.getAllRoomsFromInventory()>0);
+        assertTrue(inventoryService.getAllRoomsFromInventory()>=0);
 
     }
 
     @Test
     @DisplayName("Return the total quantity of the clues in the database")
     void testGetAllCluesFromInventory() {
-        assertTrue(inventoryService.getAllCluesFromInventory()>0);
+        assertTrue(inventoryService.getAllCluesFromInventory()>=0);
 
     }
 
     @Test
     @DisplayName("Return the total quantity of object decorations in the database")
     void testGetAllObjectDecorationsFromInventory() {
-        assertTrue(inventoryService.getAllObjectDecorationsFromInventory()>0);
+        assertTrue(inventoryService.getAllObjectDecorationsFromInventory()>=0);
     }
 
 
