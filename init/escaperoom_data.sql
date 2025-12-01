27800614c8cd9dec0c54ae226a4f91d6670b03ad
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2025-12-01 17:15:26 +0100

    UPDATED DB

--- init/escaperoom_data.sql
+++ init/escaperoom_data.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: escape_room
 -- ------------------------------------------------------
--- Server version	8.0.44
+-- Server version	8.0.448ij
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
